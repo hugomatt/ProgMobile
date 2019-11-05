e0a4290fd4b231d3f37071c45294c35be9d5da44
Hugo <mattioda@et.esiea.fr>
2019-11-05 16:47:07 +0100

    API dev
    Try to switch ListView to RecyclerView

--- app/src/main/java/com/example/progmobile/FirstFragment.java
+++ app/src/main/java/com/example/progmobile/FirstFragment.java
@@ -1,37 +1,44 @@
 package com.example.progmobile;
-// mport android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.ArrayList;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-public class FirstFragment extends Fragment /*implements AppCompatActivity*/ {
+public class FirstFragment extends Fragment {
 
     private ListView listView;
     private RetroAdapter retroAdapter;
 
-    /* @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_first);
-
-        listView = findViewById(R.id.lv);
-
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        listView = view.findViewById(R.id.lv);
         getJSONResponse();
 
+        return view;
     }
 
     private void getJSONResponse(){
@@ -94,17 +101,17 @@ public class FirstFragment extends Fragment /*implements AppCompatActivity*/ {
 
                 }
 
-                retroAdapter = new RetroAdapter(this, modelListViewArrayList);
+                retroAdapter = new RetroAdapter(getContext(), modelListViewArrayList);
                 listView.setAdapter(retroAdapter);
 
             }else {
-                Toast.makeText(FirstFragment.this, obj.optString("message")+"", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), obj.optString("message")+"", Toast.LENGTH_SHORT).show();
             }
 
         } catch ( JSONException e) {
             e.printStackTrace();
         }
 
-    }*/
+    }
 
 }
